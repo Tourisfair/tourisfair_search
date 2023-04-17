cbfb68d9d56bb2e1bc36bfb5345ccef06f9d9fd6
Selim Bayram Sutcu <bayram.selim57@gmail.com>
2023-04-17 16:27:33 +0200

    Style de carte fixé.

---
+++ script.js
@@ -0,0 +1,78 @@
+
+// RECUPERER L'ACTIVITE //
+
+function obtenirActivite() {
+  return document.getElementById("activite").value;
+}
+
+// ENVOYER UNE REQUETE //
+
+function search() {
+  let url = `https://api.tourisfair.de/api/v1/activities?page=1&limit=10`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(json => json.data)
+    .then(afficher)
+}
+
+//AFFICHER LES RESULTATS //
+function afficher(activities) {
+  console.log(activities);
+  const container = document.querySelector('.container');
+  container.innerHTML = "";
+  activities.forEach(activity => {
+    const activityDiv = document.createElement('div');
+    activityDiv.classList.add('activity');
+
+    const url = document.createElement('img');
+    url.textContent = activity.photos.urls;
+    activityDiv.appendChild(url);
+    
+    const title = document.createElement('h2');
+    title.textContent = activity.title.substring(0, 30) + '...';
+    activityDiv.appendChild(title);
+
+    
+    const adress = document.createElement('div');
+    adress.textContent = activity.adress;
+    adress.textContent = "5 rue de l'adresse, 98598";
+    adress.classList.add('address');
+    activityDiv.appendChild(adress);
+    
+    const cost = document.createElement('div');
+    cost.textContent = activity.cost;
+    cost.textContent = "€€€€€";
+    cost.classList.add('cost');
+    activityDiv.appendChild(cost);
+    
+    
+    const chip = document.createElement('div');
+    chip.textContent = activity.chip;
+    chip.textContent = "Churches";
+    chip.classList.add('chip1');	
+    activityDiv.appendChild(chip);
+    
+    const chip2 = document.createElement('div');
+    chip2.textContent = activity.chip;
+    chip2.textContent = "History";
+    chip2.classList.add('chip2');	
+    activityDiv.appendChild(chip2);
+
+    const abstractElem = document.createElement('p');
+    abstractElem.textContent = activity.abstract.substring(0, 255);
+    abstractElem.classList.add('activity-abstract');
+    activityDiv.appendChild(abstractElem);
+    
+    const btn = document.createElement('button');
+    btn.textContent = activity.btn;
+    btn.textContent = "Book Now";
+    btn.classList.add('btn');
+    activityDiv.appendChild(btn);
+    container.appendChild(activityDiv);
+  });
+
+  
+}
+
+

7090ec8289b6d4aae55b4366018090783b875843
Selim Bayram Sutcu <bayram.selim57@gmail.com>
2023-04-19 15:09:28 +0200

    tfButton developpé fix #7

--- components/tfButton.js
+++ components/tfButton.js
@@ -8,9 +8,32 @@ export class tfButton extends tfBase {
     connectedCallback(){
         this.shadowRoot.innerHTML += html`
             <link rel="stylesheet" href="/components/styles/tf-button.css" />
-            <button class="primary">
+            <button class="${this.priority}">
                 <slot></slot>
             </button>
         `;
+    }
+
+    static get observedAttributes(){
+        return ['priority']
+    }
+
+    // attributeChangedCallback(name, oldValue, newValue){
+    //     console.log('attribute changed', name, oldValue, newValue);
+    //     console.log(this.shadowRoot)
+    //     const buttonElem = this.shadowRoot.querySelector('button');
+    //     console.log(buttonElem);
+    //     if (name === 'priority'){
+    //         buttonElem.classList.remove(oldValue);
+    //         buttonElem.classList.add(newValue);
+    //     }
+    // }
+
+    get priority(){
+        return this.getAttribute('priority') || 'primary';
+    }
+
+    set priority(value){
+        this.setAttribute('priority', value);
     }
 };
